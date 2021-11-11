facd6ebfc245f8039551a59124f5469aa49515a7
tejasdesai <tejasdesai0531@gmail.com>
2021-11-11 15:59:10 +0000

    Assgining port from process.env

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,10 @@ const start = async () => {
     console.error(err);
   }
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+  const PORT = process.env.PORT || 3000
+
+  app.listen(PORT, () => {
+    console.log('Listening on port ' + PORT);
   });
 };
 
